f9210d20c16a69e6221ee70aad6f28899ae26638
8BitJake <jake8bittaylor@gmail.com>
2021-08-02 11:50:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 8bitjake.github.io
+ My website.
---
+++ assets/js/index.js
@@ -0,0 +1,107 @@
+/*
+PLEASE READ ⬇
+I know, I know, this is a terrible way to organise JS.
+I did it like this because I don't know how to use/define cross-file functions yet.
+Please do not complain about this.
+*/
+
+
+var data;
+var charData;
+var post;
+var pfp = 'https://uploads.scratch.mit.edu/users/avatars/19837061.png'
+var username = '8BitJake'
+var p;
+async function fetchAPI(url) {
+    
+  // Storing response
+  response = await fetch(url);
+  
+  // Storing data in form of JSON
+  data = await response.json();
+  console.log(data)
+  if (response) {
+      console.log('Data fetched from "' + url + '"');
+  }
+}
+(async () => {
+if (page == 'index') {
+await fetchAPI('https://my-ocular.jeffalo.net/api/user/8bitjake');
+document.getElementById('status').innerHTML = '<div id="status-dot"></div> <em title="Status taken from ocular.jeffalo.net">' + data.status + '</em>';
+let ocular = data
+await fetchAPI("https://desolate-badlands-78322.herokuapp.com/https://api.scratch.mit.edu/users/8bitjake");
+document.getElementById('status').innerHTML = data.profile.country + ' | ' + document.getElementById('status').innerHTML
+document.getElementById("status-dot").style.backgroundColor = ocular.color;
+document.getElementById('bio').innerHTML = '<strong>About Me</strong><br>' + replaceAll(data.profile.bio, '\n', '<br>')
+document.getElementById('wiwo').innerHTML = "<strong>What I'm Working On</strong><br>" + replaceAll(data.profile.status, '\n', '<br>')
+} else if (page == 'chars') {
+
+await fetchAPI('chars.json')
+for (let i = 0; i < data.charCount; i++) {
+    charData = data.chars[i]
+    document.getElementsByClassName('main')[0].innerHTML += '<div class="charBG"><span class="charName">' + charData.char.toUpperCase() + '</span>' +
+    '<br>' + charData.desc + '<br><br> <img class="charImg" src="/assets/img/char/' + charData.char + '.svg" width="' + ((charData.charWidth) ? charData.charWidth  : `25%`) +  '"height="auto" alt="' + charData.char + '"></div>'
+  let elem = document.getElementsByClassName('charName')[i];
+  elem.style.setProperty('--textColor', charData.charColor);
+  let oldElem = elem
+  elem = document.getElementsByClassName('charBG')[i];
+  if (oldElem.innerHTML != 'FOLDERY') {
+  elem.style.setProperty('--bgColor', charData.charBgColor);
+} else {
+  elem.style.background = "linear-gradient(#e8aa00, #c89200)";
+}
+  elem.style.setProperty('--divTextColor', ((charData.textColor) ? charData.textColor  : 'black'));
+  
+  }
+  
+
+} else if (page == 'blog') {
+await fetchAPI('posts.json')
+for (let i = 0; i < data.postCount; i++) {
+  postData = data.posts[i];
+createPost(i);
+document.getElementsByClassName('user')[i].innerHTML = '<img src="' + pfp + '" width="50px" height="auto" alt="' + username + '" style="border-radius: 9999px;"> <span style="position:relative; bottom:1em;">' + username + '</span>'
+document.getElementsByClassName('post')[i].innerHTML +=  '<div class="postContent"><p class="postName">' + postData.title + '</p><hr class="divider"><p class="postText">' + postData.content + '</p></div></div></a>';
+}
+} else if (page == 'post-viewer') {
+  await fetchAPI('../posts.json')
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  p = urlParams.get('p');
+  postData = data.posts[p];
+  console.log(p);
+ if (p) {
+  var postTemplate = '<a href="../">⬅ Go back</a><br><br><div class="post"><span class="user"></span><br><br></div>'
+  for (let i = data.postCount; i >= 0; i--) {
+    console.log(i)
+    console.log(i == p)
+    if (i == p) {
+  document.getElementsByClassName('main')[0].innerHTML = postTemplate;
+  document.getElementsByClassName('user')[0].innerHTML = '<img src="' + pfp + '" width="50px" height="auto" alt="' + username + '" style="border-radius: 9999px;"> <span style="position:relative; bottom:1em;">' + username + '</span>'
+document.getElementsByClassName('post')[0].innerHTML +=  '<div class="postContent"><p class="postName">' + postData.title + '</p><hr class="divider"><p class="postText">' + postData.content + '</p></div></div></a>';
+document.title += ' ' + postData.title
+    }
+  }
+
+ } else {
+   window.location.href="../"
+ }
+}
+
+document.getElementsByTagName("body")[0].innerHTML=twemoji.parse(document.getElementsByTagName("body")[0].innerHTML, { folder: 'svg', ext: '.svg'});
+
+
+
+function replaceAll(string, search, replace) {
+    return string.split(search).join(replace);
+  }
+
+  function createPost(postNum) {
+    var postTemplate = '<a href="post-viewer/?p=' + postNum + '"><div class="post"><span class="user"></span></div></a>'
+    document.getElementsByClassName('main')[0].innerHTML += postTemplate
+
+
+
+  }
+
+})()
